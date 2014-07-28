ffa6487ab6c1d3a835c4c3ac03cf126e42e32aab
Martin Broder <mrtnbroder@gmail.com>
2014-07-29 01:00:57 +0200

    add php includes folder
    for easy header/footer related changes

---
+++ src/htdocs/includes/header.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>Gulp Starter</title>
+
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+
+  <!-- Spiders must use meta description -->
+  <meta name="robots" content="noodp, noydir">
+
+  <!-- No Google Translate toolbar -->
+  <meta name="google" content="notranslate">
+
+  <!-- Viewport and mobile -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1, minimum-scale=1, minimal-ui">
+  <meta name="HandheldFriendly" content="true">
+  <meta name="MobileOptimized" content="320">
+  <meta http-equiv="cleartype" content="on">
+
+  <!-- iOS web app -->
+  <!-- Note: more options here: https://gist.github.com/tfausak/2222823 -->
+
+  <!--
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-title" content="__PAGE_TITLE__">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  -->
+
+  <!-- Icons -->
+  <!-- Note: One touch icon works but is a tad heavier (http://mathiasbynens.be/notes/touch-icons) -->
+
+  <!--
+  <link rel="apple-touch-iconprecomposed" sizes="152x152" href="assets/images/icons/apple-touch-icon-152x152.png">
+  <link rel="icon" href="assets/images/icons/favicon.png">
+  <meta name="msapplication-TileImage" content="assets/images/icons/wp-tile-icon-144x144.png">
+  <meta name="msapplication-TileColor" content="#FFFFFF">
+  -->
+
+  <link rel="stylesheet" href="app.css">
+</head>
+<body>
--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,131 +1,24 @@
-<!doctype html>
-<html class="no-js" lang="">
 
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<?php include_once "includes/header.php"; ?>
 
-  <title>Gulp Starter</title>
-
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
   <div class="page">
-
-    <section class="g">
+    <header id="header" class="g">
       <div class="g-b">
         <h1>Heading h1</h1>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-          ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
-          tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
-          ultricies mi vitae est.</p>
-        <h2>Heading h2</h2>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-          ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
-          tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
-          ultricies mi vitae est.</p>
-        <h3>Heading h3</h3>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-          ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
-          tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
-          ultricies mi vitae est.</p>
-        <h4>Heading h4</h4>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-          ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
-          tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
-          ultricies mi vitae est.</p>
-        <h5>Heading h5</h5>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-          ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
-          tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
-          ultricies mi vitae est.</p>
-        <h6>Heading h6</h6>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-          ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
-          tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
-          ultricies mi vitae est.</p>
-        <blockquote>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo.</p>
-        </blockquote>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
-    </section>
-
-    <section id="main" class="g">
-      <main id="content" class="g-b g-b--t5of8 g-b--d6of12">
-        <div class="g">
-          <div class="g-b g-b--t6of8 g-b--d3of6">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-              id est laborum.</p>
-          </div>
-          <div class="g-b g-b--t2of8 g-b--d3of6">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-              id est laborum.</p>
-          </div>
-        </div>
-      </main>
-      <aside id="sidebar" class="g-b g-b--t3of8 g-b--d6of12">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-          non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </aside>
-    </section>
-
+    </header><!-- /#header -->
     <section class="g">
-      <div class="g-b g-b--1of4">
-        <p>Sint amet nulla eiusmod magna esse excepteur. Laboris minim veniam culpa
-          non nisi. Consectetur cupidatat eu adipisicing aliquip irure adipisicing.
-          Pariatur culpa nulla irure amet irure excepteur veniam quis in labore.
-          Ex labore pariatur dolore amet irure sit commodo ipsum Lorem sint excepteur
-          tempor. Ad Lorem sint duis ad cillum eiusmod laboris ipsum qui. Excepteur
-          aliquip elit esse minim officia Lorem.</p>
-      </div>
-      <div class="g-b g-b--1of4">
-        <p>Dolor veniam minim laborum esse dolore Lorem qui duis nisi ad anim. Velit
-          nisi anim velit esse irure. Aliqua non id aliquip incididunt veniam nulla
-          tempor culpa non non. Non esse ea consectetur velit tempor incididunt irure
-          reprehenderit ex deserunt consequat.
-        </p>
-      </div>
-      <div class="g-b g-b--1of4">
-        <p>Reprehenderit pariatur dolor aute enim commodo anim. Et voluptate aute qui
-          nulla et et aute qui irure. Id fugiat proident aliquip pariatur ea laborum
-          laborum et quis aliqua ipsum Lorem culpa enim. Et consequat nisi excepteur
-          eu eu nisi ipsum nulla do duis nisi labore. Cupidatat aliqua aliquip dolore
-          voluptate eu labore nisi cillum Lorem. Ea pariatur velit ipsum deserunt
-          eiusmod non Lorem dolore irure sint sit irure.</p>
-      </div>
-      <div class="g-b g-b--1of4">
-        <p>Cillum enim non nostrud nisi cupidatat mollit. Nostrud amet laboris quis
-          est nisi amet magna commodo commodo ullamco amet. Pariatur consequat minim
-          tempor ullamco elit dolore excepteur quis officia proident. Consequat elit
-          eiusmod enim et nostrud sint nostrud cillum voluptate. Dolor et nostrud
-          sit excepteur incididunt esse cillum veniam non commodo est elit ex sit.</p>
-      </div>
-    </section>
+      <main id="main" class="g-b g-b--t5of8 g-b--d8of12 g-b--right">
+        <h2>Main h2</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </main><!-- /#main -->
 
-  </div>
-  <!-- .page-wrapper -->
-  <script src="/app.js"></script>
-</body>
+      <aside id="sidebar" role="complementary" class="g-b g-b--t3of8 g-b--d4of12">
+        <h2>Sidebar h2</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </aside><!-- /#sidebar -->
+    </section><!-- /.g -->
+  </div><!-- /.page -->
 
-</html>
+<?php include_once "includes/footer.php"; ?>
